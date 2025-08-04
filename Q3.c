b5e2315e5d6a6af84225cabddc4482c4388e1691
Nikita Gupta <nikitagpt06@gmail.com>
2025-08-04 13:45:22 +0530

    Add files via upload

---
+++ Q3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[100];
+    printf("Enter a string : ");
+    fgets(str,sizeof(str),stdin);
+    int len=strlen(str);
+    if(str[len-1]=='\n'){
+        str[len-1]='\0';
+        len--;
+    }
+    printf("Reversed string : ");
+    for(int i=len-1;i>=0;i--){
+        putchar(str[i]);
+    }
+    printf("\n");
+    return 0;
+}
